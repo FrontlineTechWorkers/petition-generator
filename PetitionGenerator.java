bc2a5a96850f0cddef27c194666f3a811c663732
JeffreyLee <jeffreyleeon@gmail.com>
2015-05-20 10:38:13 +0800

    Dealt with file io, reading names successfully

--- PetitionGenerator.java
+++ PetitionGenerator.java
@@ -1,5 +1,8 @@
 import java.util.Set;
+// File I/O
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 
 public class PetitionGenerator {
@@ -14,15 +17,37 @@ public class PetitionGenerator {
 
   public PetitionGenerator(String fileName) {
     // Open the file
+    FileInputStream fstream = null;
+    BufferedReader br = null;
     try {
-      FileInputStream fstream = new FileInputStream(fileName);
-    } catch (FileNotFoundException e) {
-      System.out.println("Names file not found, plz check if the file exist.");
+      try {
+        fstream = new FileInputStream(fileName);
+        br = new BufferedReader(new InputStreamReader(fstream));
+        String name;
+        //Read File Line By Line
+        while ((name = br.readLine()) != null)   {
+          System.out.print(name);
+          System.out.println(" " + name.length());
+        }
+      } catch (FileNotFoundException e) {
+        System.out.println("Names file not found, plz check if the file exist.");
+      } catch (Exception e) {
+        System.out.println("Error occurs while reading the name file");
+      } finally {
+        //Close the input stream
+        if (br != null) {
+          br.close();
+        }
+        if (fstream != null) {
+          fstream.close();
+        }
+      }
+    } catch (Exception e) {
+      System.out.println("Error occurs while reading the name file");
     }
   }
 
   public static void main(String[] args) {
-    System.out.println("Helo");
     PetitionGenerator pg = new PetitionGenerator();
   }
 }
